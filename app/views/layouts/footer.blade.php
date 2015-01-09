5217651dbf0b4e1811e771360a4c6e84f96beed7
Seth Ryder <ryder.seth@gmail.com>
2015-01-08 23:30:42 -0500

    remove copyright until its worded better

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 
 <footer class="footer">
   <div class="container">
-    <p class="pull-left">&copy; 2015 Modpack Index</p>
+    <p class="pull-left">Modpack Index</p>
     <p class="pull-right">Not affiliated with any Mod, Modpack, Launcher, or Mojang.</p>
   </div>
 </footer>
