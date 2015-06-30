4c994c7afd552d6e4b990c4d7d29b999b4a992f2
Seth Ryder <ryder.seth@gmail.com>
2015-06-30 19:09:10 -0400

    add api docs to the footer

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
 <footer class="footer">
   <div class="container">
     <p class="pull-left"><a href="/about">About</a> | <a href="/contact">Contact</a> |
-        <a href="https://webchat.esper.net/?channels=ModpackIndex">IRC</a></p>
+        <a href="https://modpackindex-api-docs.readthedocs.org/en/latest/">API</a> | <a href="https://webchat.esper.net/?channels=ModpackIndex">IRC</a></p>
     <p class="pull-right">Not affiliated with any Mod, Modpack, Launcher, or Mojang.</p>
   </div>
 </footer>
