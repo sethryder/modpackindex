77388326d2f9efdf83fdd99de4a5cdc082ce2e9c
Seth Ryder <ryder.seth@gmail.com>
2015-06-29 23:36:25 -0400

    remove extra / from email confirmation url

--- app/views/emails/auth/confirmation.blade.php
+++ app/views/emails/auth/confirmation.blade.php
@@ -7,7 +7,7 @@
 <h2>Account Confirmation</h2>
 
 <div>
-    <p>Someone registered an account under this email at Modpack Index. If this was you follow this link to confirm you account: <a href="{{ $site_url }}user/verify/{{ $confirmation }}" rel="nofollow">{{ $site_url }}/user/verify/{{ $confirmation }}</a></p>
+    <p>Someone registered an account under this email at Modpack Index. If this was you follow this link to confirm you account: <a href="{{ $site_url }}user/verify/{{ $confirmation }}" rel="nofollow">{{ $site_url }}user/verify/{{ $confirmation }}</a></p>
     <p>If this was not you, you can safety ignore this email. No further action is required on your part.</p>
 </div>
 </body>
