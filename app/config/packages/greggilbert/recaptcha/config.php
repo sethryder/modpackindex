a19d620ec9a3897b21d948ae5284286f5d2b724d
Seth Ryder <ryder.seth@gmail.com>
2015-07-06 18:26:01 -0400

    default config files

---
+++ app/config/packages/greggilbert/recaptcha/config.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| API Keys
+	|--------------------------------------------------------------------------
+	|
+	| Set the public and private API keys as provided by reCAPTCHA.
+	|
+	*/
+	'public_key'	=> '6LeIvfwSAAAAAFSFxE5KfvWiAtlwbEGU0NBiK7X0',
+	'private_key'	=> '6LeIvfwSAAAAAD5gDoKF8XKccWpmPIC95a79cLRV',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Template
+	|--------------------------------------------------------------------------
+	|
+	| Set a template to use if you don't want to use the standard one.
+	|
+	*/
+	'template'		=> '',
+	
+);
