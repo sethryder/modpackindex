fd82d6c4d490e03dbbd35f690d7da3703db3dea3
Seth Ryder <ryder.seth@gmail.com>
2015-07-06 18:43:28 -0400

    include a link to github in the footer

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -3,8 +3,9 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left"><a href="/about">About</a> | <a href="/contact">Contact</a> |
-            <a href="https://modpackindex-api-docs.readthedocs.org/en/latest/">API</a> | <a
-                    href="https://webchat.esper.net/?channels=ModpackIndex">IRC</a></p>
+            <a href="https://modpackindex-api-docs.readthedocs.org/en/latest/">API</a> |
+            <a href="https://github.com/sethryder/modpackindex">Source</a> |
+            <a href="https://webchat.esper.net/?channels=ModpackIndex">IRC</a></p>
 
         <p class="pull-right">Not affiliated with any Mod, Modpack, Launcher, or Mojang.</p>
     </div>
