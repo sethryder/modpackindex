ff40d38ac70e8d84274a8b453da4c62abd9f8c4f
Seth Ryder <ryder.seth@gmail.com>
2015-07-19 14:51:54 -0400

    move markdown notice to right input

--- app/views/modpacks/add.blade.php
+++ app/views/modpacks/add.blade.php
@@ -67,7 +67,6 @@
                             <div class="form-group">
                                 {{ Form::label('deck','Deck') }}:
                                 {{ Form::text('deck', null, array('class' => 'form-control'))}}
-                                <p class="pull-right">We support <a href="https://help.github.com/articles/github-flavored-markdown/" target="_blank">Github Flavored Markdown</a>.</p>
                             </div>
                             <!-- /.form-group -->
 
@@ -98,6 +97,7 @@
                             <div class="form-group">
                                 {{ Form::label('description','Description') }}:
                                 {{ Form::textarea('description', null, array('class' => 'form-control'))}}
+                                <p class="pull-right">We support <a href="https://help.github.com/articles/github-flavored-markdown/" target="_blank">Github Flavored Markdown</a>.</p>
                             </div>
                             <!-- /.form-group -->
 
