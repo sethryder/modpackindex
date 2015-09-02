a06a9ae4d804c974c55436cd1864100b5d494eac
Lee Zhe Yang <zheyanglee@gmail.com>
2015-09-02 20:43:58 +0800

    Converted some of the hard links into helper generated links. Changed output for the icon_html to an object for the datatable to sort against. Added sorting by icon to the datatable, based on icon object properties. Reformatted according to PSR-2 standard. Removed unnecessary use as I'm on ide-helper now. Reworked formatting again.

--- app/views/api/table/data.blade.php
+++ app/views/api/table/data.blade.php
@@ -6,8 +6,11 @@ var table_1 = $('#{{ $table_id }}').dataTable ({
 @if ($table_order == false)"order": [],@endif
 "sDom": '{{ $table_sdom }}',
 "oLanguage": {
-    "sEmptyTable": "{{{ $table_empty }}}",
+"sEmptyTable": "{{{ $table_empty }}}",
 },
+"aoColumnDefs": [
+{ sType: 'launcher', aTargets: [4] }
+],
 "aoColumns": [
 @foreach ($columns as $index => $column)
     { "mData": "{{ $column }}" }@if ($index+1 != count($columns)), @endif
@@ -15,8 +18,26 @@ var table_1 = $('#{{ $table_id }}').dataTable ({
 @endforeach],
 "fnInitComplete": function(oSettings, json) {
 $(this).parents ('.dataTables_wrapper').find ('.dataTables_filter input').prop ('placeholder', 'Search...').addClass ('form-control input-sm')
-}
-});
+},
+"fnRowCallback": function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+var icon_object = aData.icon_html;
+$('td:eq(4)', nRow).html("<a href='"+icon_object.link+"'>"+"<img src='"+icon_object.icon+"'></a>");
+}});
 @if ($table_fixed_header == true)new $.fn.dataTable.FixedHeader( table_1 );
 @endif
-})
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+"launcher-asc": function( a, b ) {
+var x = a.title.toLowerCase();
+var y = b.title.toLowerCase();
+
+return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+},
+
+"launcher-desc": function(a,b) {
+var x = a.title.toLowerCase();
+var y = b.title.toLowerCase();
+
+return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+}
+} );
+});
