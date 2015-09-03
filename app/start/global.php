c0a26ad7cd8656950a833201c5e6af806ca62241
Seth Ryder <ryder.seth@gmail.com>
2015-09-02 23:42:13 -0400

    setup a proper maintenance page.

--- app/start/global.php
+++ app/start/global.php
@@ -75,7 +75,7 @@ App::error(function(Exception $exception, $code)
 
 App::down(function()
 {
-	return Response::make("Be right back!", 503);
+	return Response::view('pages.down', [], 503);
 });
 
 /*
