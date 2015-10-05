eba13f474c855badb42a44864f19a2301d0084ec
Seth Ryder <ryder.seth@gmail.com>
2015-10-05 19:58:39 -0400

    include permissions with table migration

--- app/database/migrations/2014_10_29_212032_create_permissions_table.php
+++ app/database/migrations/2014_10_29_212032_create_permissions_table.php
@@ -18,6 +18,27 @@ class CreatePermissionsTable extends Migration {
             $table->string('display_name');
             $table->timestamps();
         });
+
+        DB::table('permissions')->insert([
+            ['route' => 'mod_add', 'display_name' => 'Add Mod'],
+            ['route' => 'mod_edit', 'display_name' => 'Edit Mod'],
+            ['route' => 'modpack_add', 'display_name' => 'Add Modpack'],
+            ['route' => 'modpack_edit', 'display_name' => 'Edit Modpack'],
+            ['route' => 'author_add', 'display_name' => 'Add Author'],
+            ['route' => 'author_edit', 'display_name' => 'Edit Author'],
+            ['route' => 'creator_add', 'display_name' => 'Add Creator'],
+            ['route' => 'creator_edit', 'display_name' => 'Edit Creator'],
+            ['route' => 'permissions_edit', 'display_name' => 'Edit User Permissions'],
+            ['route' => 'cache_clear', 'display_name' => 'Clear Cache'],
+            ['route' => 'modpack_code_add', 'display_name' => 'Add Modpack Code'],
+            ['route' => 'modpack_code_edit', 'display_name' => 'Edit Modpack Code'],
+            ['route' => 'mod_import', 'display_name' => 'Import Mod'],
+            ['route' => 'modpack_tag', 'display_name' => 'Add Modpack Tag'],
+            ['route' => 'youtube_add', 'display_name' => 'Add Youtube Video / Playlist'],
+            ['route' => 'modpack_alias_add', 'display_name' => 'Add Modpack Alias'],
+            ['route' => 'server_edit', 'display_name' => 'Edit Server'],
+            ['route' => 'server_tag', 'display_name' => 'Manage Server Tags'],
+        ]);
 	}
 
 	/**
